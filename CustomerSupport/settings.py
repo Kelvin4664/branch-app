aba35ce3b3ea993f6855661fb4fe530c5a7daed2
Kelvin <kelvin.adigwu@centricityinsights.com>
2021-08-03 10:29:34 +0100

    added django-heroku

--- CustomerSupport/settings.py
+++ CustomerSupport/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,3 +146,6 @@ CHANNEL_LAYERS = {
 LOGIN_REDIRECT_URL = LOGOUT_REDIRECT_URL = "/tickets/"
 LOGIN_URL = "/login/"
 STATIC_ROOT = "static"
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
